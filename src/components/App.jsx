6be210b9c0847d81a4c45060931d594fe05c0ee1
Gervis Maldonado <gervislm@icloud.com>
2023-02-23 16:12:06 -0300

    Primer commit base del proyecto

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import '../styles/global.scss';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hola Mundo</h1>
+      <p>Bienvenido al curso practico de react!</p>
+
+    </div>
+  );
+}
+
+export default App;
